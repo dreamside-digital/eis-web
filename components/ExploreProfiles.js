a2331baca3fcf9ff95d9a72eab1767a2f7b5faee
Sharon <sharon.peishan.kennedy@gmail.com>
2024-09-05 14:35:47 -0400

    better mobile layout fo rproifles

--- components/ExploreProfiles.js
+++ components/ExploreProfiles.js
@@ -51,19 +51,27 @@ export default function ExploreProfiles({profiles, tags, locale, messages }) {
       </div>
       <div className="basis-3/4">
         <div className="flex justify-between">
-          <div className="flex items-center p-2">
+          <div className="hidden md:flex items-center p-2">
             <button className={`btn text-sm px-4 py-2 w-12 h-12 rounded-full ${(currentPage <= 0) ? 'bg-slate-200 text-slate-400' : 'bg-dark text-white hover:bg-highlight'}`} onClick={decrementPage} disabled={(currentPage <= 0)}>
               <ChevronLeftIcon />
             </button>
           </div>
           <div>
-          <Accordion profiles={profilesPage} locale={locale} messages={messages} />
+            <Accordion profiles={profilesPage} locale={locale} messages={messages} />
           </div>
-          <div className="flex items-center p-2">
+          <div className="hidden md:flex items-center p-2">
             <button className={`btn text-sm px-4 py-2 w-12 h-12 rounded-full ${(pageEndIndex >= filteredProfiles.length) ? 'bg-slate-200 text-slate-400' : 'bg-dark text-white hover:bg-highlight'}`} onClick={incrementPage} disabled={(pageEndIndex >= filteredProfiles.length)}>
               <ChevronRightIcon />
             </button>
           </div>
+        </div>
+        <div className="flex md:hidden gap-2 mt-2 justify-center">
+          <button className={`btn text-sm px-4 py-2 w-12 h-12 rounded-full ${(currentPage <= 0) ? 'bg-slate-200 text-slate-400' : 'bg-dark text-white hover:bg-highlight'}`} onClick={decrementPage} disabled={(currentPage <= 0)}>
+              <ChevronLeftIcon />
+            </button>
+            <button className={`btn text-sm px-4 py-2 w-12 h-12 rounded-full ${(pageEndIndex >= filteredProfiles.length) ? 'bg-slate-200 text-slate-400' : 'bg-dark text-white hover:bg-highlight'}`} onClick={incrementPage} disabled={(pageEndIndex >= filteredProfiles.length)}>
+              <ChevronRightIcon />
+            </button>
         </div>
       </div>
     </div>
