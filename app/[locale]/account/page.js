62549923ff9de2c71a112de741711365bfbe452b
Sharon <sharon.peishan.kennedy@gmail.com>
2024-10-08 01:28:08 -0400

    account page profiles

--- app/[locale]/account/page.js
+++ app/[locale]/account/page.js
@@ -7,6 +7,13 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { profileFormFields } from '@/utils/profileFormFields'
 
+const statusColors = {
+  'draft': 'slate-100',
+  'review': 'lavendar',
+  'private': 'light',
+  'published': 'medium'
+}
+
 
 export default function AccountPage({params: {locale}}) {
   const [user, setUser] = useState()
@@ -39,49 +46,52 @@ export default function AccountPage({params: {locale}}) {
         <div className="bg-[url(/images/Explore_Culture_Vicinity_BG.png)] bg-no-repeat bg-cover absolute top-0 left-0 h-2/3 w-full">
         </div>
         <div className="container max-w-screen-lg mx-auto relative flex justify-center pt-6">
-          <div className="p-6 w-full bg-light text-dark rounded-xl">
+          <div className="p-6 w-full bg-lavendar text-dark rounded-xl">
             <h1 className="uppercase text-3xl mb-4 md:mb-6 font-medium">Account</h1>
             <p>{`${user?.first_name} ${user?.last_name}`}</p>
             <p>{user?.email}</p>
           </div>
         </div>
       </section>
-      <section className="bg-white text-dark relative">
-        <div className="container max-w-screen-lg mx-auto px-6 mb-12 lg:mb-20">
+      <section className="bg-white text-dark relative p-6">
+        <div className="container max-w-screen-lg mx-auto mb-12 lg:mb-20">
           <h2 className="uppercase text-2xl mb-4 md:mb-6 font-medium">Artist profiles</h2>
           <div className="grid grid-cols-3 gap-6">
             {profiles?.map(profile => {
               const tagsText = profile.tags.map(t => t.name).join(", ")
               return (
-                  <div key={profile.id} className="p-6 bg-light text-dark relative">
-                    <Link className="text-xl no-underline hover:text-highlight" href={`/${locale}/profiles/${profile.slug}`}>
-                      <h1 className="font-title text-xl md:text-2xl mb-4">
-                        {profile.public_name}
-                      </h1>
-                    </Link>
-                    <p className="mb-4">{profile.pronouns}</p>
-                    <div className="">
-                      {
-                        profile.profile_picture &&
-                        <Image
-                          className="relative w-full h-auto aspect-video object-cover rounded-xl mb-4"
-                          src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/${profile.profile_picture}`}
-                          alt={profile.public_name} 
-                          width={500}
-                          height={500}
-                        />
-                      }
-                      <div className="flex-1">
-                        <p className="mb-4">{profile.short_introduction}</p>
-                      </div>
-
-                      <div className="flex-1 mb-4">
-                        {(profile.tags.length > 0) && <p className="uppercase font-medium tracking-wide text-sm">{`${messages.tags}: ${tagsText}`}</p>}
-                      </div>
-
-                      <Link href={`/${locale}/profiles/${profile.slug}`} className="font-medium underline">
-                        {messages.full_profile}
+                  <div key={profile.id} className={`border-2 ${profile.status === "draft" && "border-slate-200"} ${profile.status === "published" && "border-medium"} text-dark relative rounded-xl`}>
+                    <div className={`${profile.status === "draft" && "bg-slate-200"} ${profile.status === "published" && "bg-medium"} text-dark px-6 py-2 uppercase font-medium text-sm`}>{profile.status}</div>
+                    <div className="p-6">
+                      <Link className="text-xl no-underline hover:text-highlight" href={`/${locale}/profiles/${profile.slug}`}>
+                        <h1 className="font-title text-xl md:text-2xl mb-4">
+                          {profile.public_name}
+                        </h1>
                       </Link>
+                      <p className="mb-4">{profile.pronouns}</p>
+                      <div className="">
+                        {
+                          profile.profile_picture &&
+                          <Image
+                            className="relative w-full h-auto aspect-video object-cover rounded-xl mb-4"
+                            src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/${profile.profile_picture}`}
+                            alt={profile.public_name} 
+                            width={500}
+                            height={500}
+                          />
+                        }
+                        <div className="flex-1">
+                          <p className="mb-4">{profile.short_introduction}</p>
+                        </div>
+
+                        <div className="flex-1 mb-4">
+                          {(profile.tags.length > 0) && <p className="uppercase font-medium tracking-wide text-sm">{`${messages.tags}: ${tagsText}`}</p>}
+                        </div>
+
+                        <Link href={`/${locale}/profiles/${profile.slug}`} className="font-medium underline">
+                          {messages.full_profile}
+                        </Link>
+                      </div>
                     </div>
                   </div>
               )
