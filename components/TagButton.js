fbc39f609cd6ec5715f0c85cd5bff80b8d08d2ff
Sharon <sharon.peishan.kennedy@gmail.com>
2024-11-14 11:02:44 -0500

    change tag button bg

--- components/TagButton.js
+++ components/TagButton.js
@@ -3,9 +3,9 @@ export default function TagButton({ tag, isSelected, onClick=null }) {
 
   if (clickable) {
     return (
-      <button className={`${isSelected ? 'bg-dark text-white' : ''} ${(isSelected) ? 'hover:bg-highlight' : 'hover:bg-primary'} border border-dark px-2 inline-flex nowrap items-center`} onClick={onClick}>
+      <button className={`hover:bg-primary border border-dark px-2 inline-flex nowrap items-center`} onClick={onClick}>
         { isSelected ? (
-            <span className="h-4 w-4 rounded-full bg-orange border border-white mr-1" />
+            <span className="h-4 w-4 rounded-full bg-orange mr-1" />
           ) : (
             <span className="h-4 w-4 rounded-full border border-dark bg-white mr-1" /> 
           )
