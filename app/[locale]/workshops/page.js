702e9adff07cda4fe8b524b49c87a2574fd8a610
Sharon <sharon.peishan.kennedy@gmail.com>
2024-12-20 11:44:16 -0500

    add captions to workshop images

--- app/[locale]/workshops/page.js
+++ app/[locale]/workshops/page.js
@@ -17,43 +17,63 @@ export default async function WorkshopPage({params: {locale}}) {
         <h1 className="font-title text-4xl md:text-6xl lg:text-7xl mb-6 md:mb-12">{translation.page_title}</h1>
         <div className="md:grid grid-cols-2 gap-x-24 gap-y-12">
           <div className="page-content wysiwyg-content max-w-prose col-span-2 max-md:my-6 text-xl xl:text-2xl" dangerouslySetInnerHTML={{ __html: para1 }} />
-          <Image
-            className="col-span-2 w-full"
-            src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/${image_1.id}`}
-            width={image_1.width}
-            height={image_1.height}
-            alt={image_1.description}
-          />
+          <div className="col-span-2 w-full">
+            <Image
+              className=""
+              src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/${image_1.id}`}
+              width={image_1.width}
+              height={image_1.height}
+              alt={image_1.description || ""}
+            />
+           {image_1.description && <p className="py-3 text-slate-500">{image_1.description}</p>}
+          </div>
           <div className="page-content wysiwyg-content max-w-prose max-md:my-6 text-xl xl:text-2xl" dangerouslySetInnerHTML={{ __html: para2 }} />
-          <Image
-            className="max-w-screen-lg w-full"
-            src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/${image_2.id}`}
-            width={image_2.width}
-            height={image_2.height}
-            alt={image_2.description}
-          />
+          
+          <div className="max-w-screen-lg w-full">
+            <Image
+              className=""
+              src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/${image_2.id}`}
+              width={image_2.width}
+              height={image_2.height}
+              alt={image_2.description || ""}
+            />
+            {image_2.description && <p className="py-3 text-slate-500">{image_2.description}</p>}
+          </div>
+          
           <div className="page-content wysiwyg-content max-w-prose col-span-2 max-md:my-6 text-xl xl:text-2xl" dangerouslySetInnerHTML={{ __html: para3 }} />
+          
+          <div className="">
+            <Image
+              src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/${image_3.id}`}
+              width={image_3.width}
+              height={image_3.height}
+              alt={image_3.description || ""}
+            />
+            {image_3.description && <p className="py-3 text-slate-500">{image_3.description}</p>}
+          </div>
+
+          <div className="mt-12 md:mt-48 lg:mt-60">
           <Image
-            src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/${image_3.id}`}
-            width={image_3.width}
-            height={image_3.height}
-            alt={image_3.description}
-          />
-          <Image
-            className="mt-12 md:mt-48 lg:mt-60"
+            className=""
             src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/${image_4.id}`}
             width={image_4.width}
             height={image_4.height}
-            alt={image_4.description}
+            alt={image_4.description || ""}
           />
+          {image_4.description && <p className="py-3 text-slate-500">{image_4.description}</p>}
+          </div>
         </div>
         <div className="page-content wysiwyg-content max-w-prose max-md:my-6 my-12 text-xl xl:text-2xl" dangerouslySetInnerHTML={{ __html: para4 }} />
-        <Image
-          src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/${image_5.id}`}
-          width={image_5.width}
-          height={image_5.height}
-          alt={image_5.description}
-        />
+        
+        <div className="">
+          <Image
+            src={`${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/${image_5.id}`}
+            width={image_5.width}
+            height={image_5.height}
+            alt={image_5.description || ""}
+          />
+          {image_5.description && <p className="py-3 text-slate-500">{image_5.description}</p>}
+        </div>
       </div>
     </section>
   )
