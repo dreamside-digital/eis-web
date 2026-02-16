cabc6db20b50dc650301436fd63bd78b30efe319
Benjamin Lappalainen <b.m.lappalainen@gmail.com>
2026-02-16 12:44:53 -0500

    Backfill palettes when serving artbox/discover

--- app/[locale]/artbox/discover/page.js
+++ app/[locale]/artbox/discover/page.js
@@ -1,9 +1,12 @@
 import ColorPaletteTool from '@/components/artbox/ColorPaletteTool'
+import { backfillMissingPalettes } from '@/lib/data-access'
 
 export const revalidate = 60
 
 export default async function DiscoverPage({ params }) {
   const { locale } = await params
+
+  await backfillMissingPalettes()
 
   return (
     <section className="bg-light text-dark min-h-screen">
